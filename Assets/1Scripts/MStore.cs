78a1c881169fc43a00d7707d91695011617cb73b
jungwoo5042 <42081803+TrollShin@users.noreply.github.com>
2020-06-19 15:05:11 +0900

    Revert "Merge branch 'master' of https://github.com/TrollShin/MarTruck"
    This reverts commit 72140e51fd7f1128ae61c0d4512c06b0527c87d3, reversing
    changes made to 2396c9ccd4553b777ca49c77ed54bf0f693e1ba7.

--- Assets/1Scripts/MStore.cs
+++ Assets/1Scripts/MStore.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public struct SStoreInfo
 {
-    public List<SQuest> QuestList;
-    public EStoreLV StoreLV;
+    List<SQuest> QuestList;
+    EStoreLV StoreLV;
 
-    public SStoreInfo(List<SQuest> QuestList, EStoreLV StoreLV)
+    SStoreInfo(List<SQuest> QuestList, EStoreLV StoreLV)
     {
         this.QuestList = QuestList.ToArray().Clone() as List<SQuest>;
         this.StoreLV = StoreLV;
     }
 
-    public SStoreInfo(SStoreInfo StoreInfo)
+    SStoreInfo(SStoreInfo StoreInfo)
     {
         this.QuestList = StoreInfo.QuestList;
         this.StoreLV = StoreInfo.StoreLV;
@@ -29,54 +29,15 @@ public enum EStoreLV
 
 public class MStore : MonoBehaviour
 {
-    private SStoreInfo StoreInfo;
-
-    public GameObject[] Car;
-    public GameObject[] Store;
-
-    public MDriftCamera driftCamera;
-    private void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-        GameInit();
-    }
-    private void GameInit()
-    {
-        //if(게임 세이브가 존재할경우)
-        //StoreInfo = (gameData)
-
-        //임시
-        List<SQuest> q = new List<SQuest>();
-        StoreInfo.QuestList = q;
-        StoreInfo.StoreLV = EStoreLV.MomNPopStore;
-
-        GameObject curCar = Car[(int)StoreInfo.StoreLV];
-
-        curCar.SetActive(true);
-
-        SetCamera(curCar);
-
-        Store[(int)StoreInfo.StoreLV].SetActive(true);
+        
     }
 
-    private void SetCamera(GameObject Car)
+    // Update is called once per frame
+    void Update()
     {
-        Transform CamRig = Car.transform.Find("CamRig");
-
-        driftCamera.lookAtTarget = CamRig.Find("CamLookAtTarget");
-        driftCamera.positionTarget = CamRig.Find("CamPosition");
-        driftCamera.sideView = CamRig.Find("CamSidePosition");
-    }
-
-    private void StoreUpgrade()
-    {
-        Car[(int)StoreInfo.StoreLV].SetActive(false);
-        Store[(int)StoreInfo.StoreLV].SetActive(false);
-
-        StoreInfo.StoreLV += 1;
-
-        Car[(int)StoreInfo.StoreLV].SetActive(true);
-        Store[(int)StoreInfo.StoreLV].SetActive(true);
-
-        SetCamera(Car[(int)StoreInfo.StoreLV]);
+        
     }
 }
