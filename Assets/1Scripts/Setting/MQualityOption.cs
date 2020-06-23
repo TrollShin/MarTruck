e154e2635fc9052256e42ca52e108922a29d16dd
Posiya <nknk610@gmail.com>
2020-06-23 11:02:00 +0900

    Add Option prefabs

---
+++ Assets/1Scripts/Setting/MQualityOption.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MQualityOption : MonoBehaviour
+{
+    public Dropdown DropdownOption;
+
+    private int _QualityLevel;
+    private int QualityLevel
+    {
+        get { return _QualityLevel; }
+        set { QualitySettings.SetQualityLevel(_QualityLevel); }
+    }
+
+    private void Awake()
+    {
+        _QualityLevel = QualitySettings.GetQualityLevel();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<Dropdown.OptionData> Options = new List<Dropdown.OptionData>();
+        foreach(string Option in typeof(EGraphicQuality).GetEnumNames())
+        {
+            Options.Add(new Dropdown.OptionData(Option));
+        }
+        DropdownOption.AddOptions(Options);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
