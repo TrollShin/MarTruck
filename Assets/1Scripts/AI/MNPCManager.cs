1e1d78a18dc7948a3e3bef05704f77fead98c1b2
jungwoo5042 <42081803+TrollShin@users.noreply.github.com>
2020-06-29 16:06:02 +0900

    AI Traffic Light

--- Assets/1Scripts/AI/MNPCManager.cs
+++ Assets/1Scripts/AI/MNPCManager.cs
@@ -4,21 +4,35 @@ using UnityEngine;
 
 public class MNPCManager : MonoBehaviour
 {
-    public MNPCArea DestinationArea;
+    [SerializeField]
+    private MNPCArea DestinationArea;
 
-    public Transform ParentAgentObject;
+    [SerializeField]
+    private Transform ParentAgentObject;
+
+    [SerializeField]
+    private MTrafficLightSignal TrafficLightSignal;
+
+    private MNPCWalker[] MNPCWalkers;
 
     private void Start()
     {
         DestinationArea.StartCoroutine();
 
-        MNPCWalker Walker;
+        TrafficLightSignal.OnSignalChanged += OnSignalChange;
 
-        for (int i = 0; i < ParentAgentObject.childCount; i++)
+        MNPCWalkers = new MNPCWalker[ParentAgentObject.childCount];
+
+        for (int i = 0; i < MNPCWalkers.Length; i++)
         {
-            Walker = ParentAgentObject.GetChild(i).GetComponent<MNPCWalker>();
-            Walker.OnAgentArrive += ChangeNpcDestination;
+            MNPCWalkers[i] = ParentAgentObject.GetChild(i).GetComponent<MNPCWalker>();
+            MNPCWalkers[i].OnAgentArrive += ChangeNpcDestination;
         }
+    }
+
+    private void OnDestroy()
+    {
+        TrafficLightSignal.OnSignalChanged -= OnSignalChange;
     }
 
     private Transform GetDestination()
@@ -27,13 +41,35 @@ public class MNPCManager : MonoBehaviour
         return Destinaitons[Random.Range(0, Destinaitons.Length)];
     }
 
-    private void ChangeNpcDestination(MNPCWalker Walker)
+    private void ChangeNpcDestination(MNPCWalker _Walker)
     {        
-        if(Vector3.Distance(Walker.transform.position, DestinationArea.transform.position) > DestinationArea.Radius)
+        if(Vector3.Distance(_Walker.transform.position, DestinationArea.transform.position) > DestinationArea.Radius)
         {
-            Walker.transform.position = GetDestination().position;
+            _Walker.transform.position = GetDestination().position;
         }
 
-        Walker.SetDestination(GetDestination(), (Random.Range(0,4)) < 1);
+        _Walker.SetDestination(GetDestination(), (Random.Range(0,4)) < 1);
+    }
+
+    //신호는 차량기준
+    private void OnSignalChange(ETrafficLightState _Signal)
+    {
+        Debug.Log("In MNPC Manager" + _Signal);
+        if (_Signal == ETrafficLightState.Red)
+        {
+            ChangeAllAgentsAreaMask(EAreaMask.Walkable_CrossWalk);
+        }
+        else
+        {
+            ChangeAllAgentsAreaMask(EAreaMask.Walkable);
+        }
+    }
+
+    private void ChangeAllAgentsAreaMask(EAreaMask _AreaMask)
+    {
+        for(int i = 0; i < MNPCWalkers.Length; i++)
+        {
+            MNPCWalkers[i].ChangeAreaMask(_AreaMask);
+        }
     }
 }
