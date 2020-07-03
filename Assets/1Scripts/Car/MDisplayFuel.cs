826f9f568661a01c8bd700a08725189873ee2a37
Posiya <nknk610@gmail.com>
2020-07-03 20:49:35 +0900

    Add PlayerState

---
+++ Assets/1Scripts/Car/MDisplayFuel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MDisplayFuel : MonoBehaviour
+{
+    public float FuelRatio
+    {
+        set
+        {
+            FuelImage.fillAmount = value;
+        }
+    }
+
+    [SerializeField]
+    private Image FuelImage;
+
+}
