466be39dad9899beb011728d13d5b2a1ecca873f
Posiya <nknk610@gmail.com>
2020-07-07 12:08:27 +0900

    Add New game and Continue in Title scene

--- Assets/1Scripts/Title/MTitleFunctionLibrary.cs
+++ Assets/1Scripts/Title/MTitleFunctionLibrary.cs
@@ -1,17 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MTitleFunctionLibrary : MonoBehaviour
 {
+    public GameObject CheckDataDeleteUI;
+    
+    private void Awake()
+    {
+        CheckDataDeleteUI.SetActive(false);
+        CSaveGame SaveGame = CSaveGame.GetInstance();
+        if (!SaveGame.LoadControlSetting())
+        {
+            SaveGame.Save();
+            SaveGame.LoadControlSetting();
+        }
+        if (!SaveGame.LoadSoundInfo()) 
+        {
+            SaveGame.Save();
+            SaveGame.LoadSoundInfo();
+        }
+    }
+
+    public void ShowCheckDataDeleteUI()
+    {
+        CheckDataDeleteUI.SetActive(true);
+    }
+
+    public void HideCheckDataDeleteUI()
+    {
+        CheckDataDeleteUI.SetActive(false);
+    }
+
     public void OnClickNewGame()
     {
-
+        CSaveGame SaveGame = CSaveGame.GetInstance();
+        CUserInfo UserInfo = CUserInfo.GetInstance();
+        UserInfo = new CUserInfo();
+        SaveGame.Save();
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
     }
 
     public void OnClickContinue()
     {
+        CSaveGame SaveGame = CSaveGame.GetInstance();
+        if(!SaveGame.LoadUserInfo())
+        {
+            SaveGame.Save();
+            SaveGame.LoadUserInfo();
+        }
 
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
     }
 
     public void OnClickSetting()
